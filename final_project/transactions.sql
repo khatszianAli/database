5e5b08c8dd868da1ff66e5ad39fa9f2a7e59f13a
khatszianAli <144136875+khatszianAli@users.noreply.github.com>
2025-12-12 16:41:52 +0600

    Adding more transactions

--- final_project/transactions.sql
+++ final_project/transactions.sql
@@ -1,13 +1,44 @@
+-- Enrolling a Student
 BEGIN;
 
-INSERT INTO student (student_name, department, student_email)
-VALUES ('Temporary Student', 'Test Dept', 'temp@test.com');
+-- Verify that the student exists
+SELECT * FROM student WHERE student_id = 1 FOR SHARE;
 
-ROLLBACK;  
+-- Verify the course exists
+SELECT * FROM course WHERE course_id = 3 FOR SHARE;
 
-BEGIN;
-
-INSERT INTO student (student_name, department, student_email)
-VALUES ('Real Student', 'CS', 'real@test.com');
+-- Insert enrollment
+INSERT INTO enrollment (student_id, course_id, grade)
+VALUES (1, 3, 'N/A');
 
 COMMIT;
+
+-- Student Submits
+BEGIN;
+
+-- Add submission
+INSERT INTO submission (assignment_id, student_id, obtained_mark)
+VALUES (6, 1, 98)
+RETURNING submission_id;
+
+-- Update enrollment grade
+UPDATE enrollment
+SET grade = 'A'
+WHERE student_id = 1 AND course_id = 1;
+
+COMMIT;
+
+-- Instructor Creates a New Course && assignment
+BEGIN;
+
+-- Create the course
+INSERT INTO course (course_name, description, credit, instructor_id)
+VALUES ('Data Structures', 'Intro to Data Structures', 6, 1)
+RETURNING course_id;
+
+-- Create assignment for the course
+INSERT INTO assignment (course_id, title, deadline, total_mark)
+VALUES (currval('course_course_id_seq'), 'First Homework', '2025-12-10 23:59:00', 100);
+
+COMMIT;
+
